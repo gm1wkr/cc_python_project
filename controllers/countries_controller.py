0f21d70e4d59e9da4cace95285573836e9f43f0f
GM1WKR <gm1wkr@gmail.com>
2022-06-11 20:31:03 +0100

    countries/show

--- controllers/countries_controller.py
+++ controllers/countries_controller.py
@@ -10,10 +10,14 @@ countries_blueprint = Blueprint("countries", __name__)
 @countries_blueprint.route("/countries", methods=["GET"])
 def countries():
     countries = country_repository.select_all()
-    return render_template("countries/index.html")
+    return render_template("countries/index.html", all_countries=countries)
 
 # SHOW 
 # GET '/countries/<id>'
+@countries_blueprint.route("/countries/<id>")
+def show(id):
+    found_country = country_repository.select(id)
+    return render_template("countries/show.html", country=found_country)
 
 
 
