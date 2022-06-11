818e26f6f58005cd9d53f08cbb53419682578b3a
GM1WKR <gm1wkr@gmail.com>
2022-06-11 17:52:03 +0100

    bootstrapped /countries route

---
+++ controllers/countries_controller.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, Flask, render_template, request, redirect
+
+from models.countries import Country
+import repositories.country_repository as country_repository
+
+countries_blueprint = Blueprint("countries", __name__)
+
+# INDEX
+# GET '/countries'
+@countries_blueprint.route("/countries", methods=["GET"])
+def countries():
+    countries = country_repository.select_all()
+    return render_template("countries/index.html")
