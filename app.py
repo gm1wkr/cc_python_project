cf023ce40a77a039acc4acf7d98b2a3f491c88ba
GM1WKR <gm1wkr@gmail.com>
2022-06-13 23:17:15 +0100

    INDEX route stats dashboard started

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ from controllers.cities_controller import cities_blueprint
 from controllers.attractions_controller import attractions_blueprint
 from controllers.notes_controller import notes_blueprint
 
+import repositories.country_repository as country_repository
+import repositories.city_repository as city_repository
 import repositories.attractions_repository as attractions_repository
+import repositories.notes_repository as notes_repository
 
 app = Flask(__name__)
 
@@ -20,8 +23,13 @@ def main():
 
     unvisited = attractions_repository.get_unvisited_attractions(),
     visited = attractions_repository.get_visited_attractions()
+    stats = {
+        "num_notes":notes_repository.get_number_of_notes(),
+        "num_cities":city_repository.get_number_of_cities(),
+        "num_countries":country_repository.get_number_of_countries()
+    }
 
-    return render_template("index.html", visited=visited, unvisited=unvisited)
+    return render_template("index.html", visited=visited, unvisited=unvisited, stats_dict=stats)
 
 if __name__ == "__main__":
     app.run()
